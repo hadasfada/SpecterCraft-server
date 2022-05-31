5f8c619f916e4d271e566158efcc2c501e02a716
hadasfada <68920407+hadasfada@users.noreply.github.com>
2022-05-31 12:25:08 -0700

    Unmodified v1

---
+++ st.bat
@@ -0,0 +1,3 @@
+java -Xmx7G -Xms2G -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=100 -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:G1MixedGCLiveThresholdPercent=35 -XX:+AlwaysPreTouch -XX:+ParallelRefProcEnabled -Dusing.aikars.flags=mcflags.emc.gs -jar mohist.jar
+pause
+@echo off
